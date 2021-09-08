ca6792b6371be994cd756da2e22f4fe349b9798d
Alwin Joseph <alwin.joseph0505@gmail.com>
2021-09-08 14:30:08 +0530

    accepts message

--- app.py
+++ app.py
@@ -8,15 +8,21 @@ def index():
     if request.method == "POST":
         print("FORM DATA RECEIVED")
         
-        if "file" not in request.files:
+        if "file" not in request.files and "message" not in request.form.get:
             return redirect(request.url)
         
         file = request.files["file"]
-        if file.filename == "":
+        message = request.form.get("message")
+        if file.filename == "" and message == "":
             return redirect(request.url)
-
+        
         save_path = os.path.join("", "temp.wav")
         file.save(save_path)
+        if file:
+            print("Audio File received")
+        if message:
+                print("Received message: ", message)
+        
             
     return render_template("index.html")
 
